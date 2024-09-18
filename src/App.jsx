2406e19eb5232897dbdd94482ae2118eaaad02c4
Deepak Chauhan <imdeepakchauhan12@gmail.com>
2024-09-18 23:56:16 +0530

    add pizza project files

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Home from "./ui/Home.jsx";
+import Menu, {loader as menuLoader} from "./features/menu/Menu.jsx";
+import Cart from "./features/cart/Cart.jsx";
+import Order, {loader as OrderLoader} from "./features/order/Order.jsx";
+import CreateOrder, {action as createOrderAction} from "./features/order/CreateOrder.jsx";
+import AppLayout from "./ui/AppLayout.jsx";
+import Error from "./ui/Error.jsx";
+
+const router = createBrowserRouter([
+    {
+        element: <AppLayout/>,
+        errorElement: <Error />,
+
+        children: [
+            {
+                path: '/',
+                element: <Home/>,
+            },
+            {
+                path: '/menu',
+                element: <Menu/>,
+                loader: menuLoader,
+                errorElement: <Error />,
+            },
+            {
+                path: '/cart',
+                element: <Cart/>
+            },
+            {
+                path: '/order/new',
+                element: <CreateOrder/>,
+                action: createOrderAction
+            },
+            {
+                path: '/order/:orderId',
+                element: <Order/>,
+                loader: OrderLoader
+            }
+        ]
+    },
+])
+
+function App() {
+    return <RouterProvider router={router}/>
+}
+
+export default App;
