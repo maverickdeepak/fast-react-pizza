79a46cc38b3ed10d3b5190dec07e115f661c0fd0
Deepak Chauhan <imdeepakchauhan12@gmail.com>
2024-09-20 23:51:12 +0530

    add favicon and design order section

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -4,13 +4,14 @@ import {
   calcMinutesLeft,
   formatCurrency,
   formatDate,
-} from "../../utils/helpers";
-import {getOrder} from "../../services/apiRestaurant.js";
-import {useLoaderData} from "react-router-dom";
+} from '../../utils/helpers';
+import { getOrder } from '../../services/apiRestaurant.js';
+import { useLoaderData } from 'react-router-dom';
+import OrderItem from './OrderItem.jsx';
 
 function Order() {
-  const order = useLoaderData()
-  console.log(order)
+  const order = useLoaderData();
+  console.log(order);
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
     id,
@@ -24,35 +25,56 @@ function Order() {
   const deliveryIn = calcMinutesLeft(estimatedDelivery);
 
   return (
-    <div>
-      <div>
-        <h2>Status</h2>
-
-        <div>
-          {priority && <span>Priority</span>}
-          <span>{status} order</span>
+    <div className="space-y-8 px-4 py-6">
+      <div className="gp-2 flex flex-wrap items-center justify-between capitalize">
+        <h2 className="text-sl font-semibold">Order #{id} status</h2>
+        <div className="space-x-2 capitalize">
+          {priority && (
+            <span className="rounded-full bg-orange-500 px-3 py-1 text-sm font-semibold uppercase tracking-wide text-orange-50">
+              Priority
+            </span>
+          )}
+          <span className="rounded-full bg-green-500 px-3 py-1 text-sm font-semibold uppercase tracking-wide text-green-50">
+            {status} order
+          </span>
         </div>
       </div>
 
-      <div>
-        <p>
+      <div className="flex flex-wrap items-center justify-between gap-2 rounded-md bg-stone-200 px-6 py-5 capitalize">
+        <p className="font-medium">
           {deliveryIn >= 0
             ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
-            : "Order should have arrived"}
+            : 'Order should have arrived'}
         </p>
-        <p>(Estimated delivery: {formatDate(estimatedDelivery)})</p>
+        <p className="text-xs text-stone-500">
+          (Estimated delivery: {formatDate(estimatedDelivery)})
+        </p>
       </div>
 
-      <div>
-        <p>Price pizza: {formatCurrency(orderPrice)}</p>
-        {priority && <p>Price priority: {formatCurrency(priorityPrice)}</p>}
-        <p>To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}</p>
+      <ul className="divide-y divide-stone-200 border-b border-t">
+        {cart.map((item) => (
+          <OrderItem key={item.pizzaId} item={item} />
+        ))}
+      </ul>
+
+      <div className="space-y-2 rounded-md bg-stone-200 px-6 py-5">
+        <p className="text-sm font-medium text-stone-600">
+          Price pizza: {formatCurrency(orderPrice)}
+        </p>
+        {priority && (
+          <p className="text-sm font-medium text-stone-600">
+            Price priority: {formatCurrency(priorityPrice)}
+          </p>
+        )}
+        <p className="font-bold">
+          To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
+        </p>
       </div>
     </div>
   );
 }
 
-export async function loader ({params}) {
+export async function loader({ params }) {
   const order = await getOrder(params.orderId);
   return order;
 }
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <header className="flex items-center justify-between border-b border-stone-500 bg-violet-500 px-4 py-3 uppercase sm:px-6">
       <Link to={'/'} className="tracking-widest">
-        Fast React Pizza Co.
+        Pizzaria.
       </Link>
       <SearchOrder />
       <Username />
